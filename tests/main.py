8672428a221b5897d5c1158c733c82c6429ec8ea
unknown <martin.bellot@epitech.eu>
2023-11-13 14:49:21 +0100

    Questionnaire

--- tests/main.py
+++ tests/main.py
@@ -3,29 +3,55 @@ from instabot import Bot
 import time
 import random
 from check_proxies import check_proxy
+import os
 
 username = "ibrahima.gnf@gmail.com"
 password = "m5Mey5eydw"
+
+os.remove(f"config/{username}_uuid_and_cookie.json")
+
 personne_cible = "simulegal"
 
+
+list_followers = False
+
+if (os.path.exists(f"data/{personne_cible}.txt")):
+    value = input("La liste des followers a été trouvé, voulez-vous RE-lister les followers (cela peut-être long dû au délai entre les requêtes) ? (oui, non): ")
+    if (value.startswith("oui")):
+        list_followers = True
+else:
+    with open(f"data/{personne_cible}.txt", "w") as f:
+        f.write("FICHIER VIDE !")
+
+print("[INFO] Get valid proxies...")
 with open("proxies/valid_proxies.txt", "r") as f:
     proxies = f.read().split('\n')
+print("[INFO] Proxies got...")
 
 bot = Bot()
 
+print("[INFO] Login...")
 bot.login(username=username, password=password)
 
 print("CONNEXION Effectuée !")
 
-followers = bot.get_user_followers(personne_cible)
+if (list_followers):
+    followers = bot.get_user_followers(personne_cible)
 
-with open("data/followers.txt", "w") as file:
-    for f in followers:
-        file.write(f"{f}\n")
+    with open(f"data/{personne_cible}.txt", "w") as file:
+        for f in followers:
+            file.write(f"{f}\n")
+    print(f"[INFO] followers saved in data/{personne_cible}.txt")
+else:
+    followers = []
+    with open(f"data/{personne_cible}.txt", "r") as file:
+        followers.append(file.read().split("\n"))
+    print("[INFO] followers got in file.")
+
 
 nb_followers = len(followers)
 
-print("[INFO] followers saved in data/followers.txt")
+
 save_in_sheet = input("Sauvegarder dans Google Sheet ? (oui, non): ")
 
 if (save_in_sheet.startswith("oui")):
